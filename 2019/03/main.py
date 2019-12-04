2c6fc672281271befaf32c9ab776f1f1fdec7d3c
hejo89 <johannes.hess.89@googlemail.com>
2019-12-04 18:16:05 +0100

    solution for day 3

--- 2019/03/main.py
+++ 2019/03/main.py
@@ -1,8 +1,18 @@
 with open("input.txt", "r") as f:
     a_wire = f.readline().strip().split(",")
     b_wire = f.readline().strip().split(",")
-a_wire = ["R8","U5","L5","D3"]
-b_wire = ["U7","R6","D4","L4"]
+#a_wire = ["R8","U5","L5","D3"]
+#b_wire = ["U7","R6","D4","L4"]
+
+#a_wire = ["R75","D30","R83","U83","L12","D49","R71","U7","L72"]
+#b_wire = ["U62","R66","U55","R34","D71","R55","D58","R83"]
+
+#a_wire = ["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"]
+#b_wire = ["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"]
+
+#a_wire = ["R100","U150"]
+#b_wire = ["U100","R150"]
+
 a_positions = [(0, 0)]
 b_positions = [(0, 0)]
 
@@ -35,28 +45,15 @@ for i in test:
             a, b, c, d =  i[1][0] - j[1][0]  , i[1][1] - j[1][1], i[2][0] - j[2][0], i[2][1] - j[2][1]
             if a*b < 0 and c*d <0:
                
-                if i[0][0] == "L" or  i[0][0] == "R":m = min(m,  abs(i[1][0]) + abs(j[2][0])); print i, j, (i[1][0] , j[2][0]), a_positions[i[3]], b_positions[j[3]]; wire_length.append(sum([int(x[1:]) for x in a_wire[:i[3]]]))
+                if i[0][0] == "L" or  i[0][0] == "R":m = min(m,  abs(i[1][0]) + abs(j[2][0])); l = sum([int(x[1:]) for x in a_wire[:i[3]]] + [int(x[1:]) for x in b_wire[:j[3]]] + [abs(a_positions[i[3]][0] - i[1][0]) + abs(a_positions[i[3]][1] - j[2][0])] + [abs(b_positions[j[3]][0] - i[1][0]) + abs(b_positions[j[3]][1] - j[2][0])]); wire_length.append(l)
 
-                if i[0][0] == "U" or  i[0][0] == "D":m = min(m,  abs(i[2][0]) + abs(j[1][0])); print i, j, (i[2][0] , j[1][0]), a_positions[i[3]], b_positions[j[3]]
+                if i[0][0] == "U" or  i[0][0] == "D":m = min(m,  abs(i[2][0]) + abs(j[1][0])); l = sum([int(x[1:]) for x in a_wire[:i[3]]] + [int(x[1:]) for x in b_wire[:j[3]]] + [abs(a_positions[i[3]][0] - j[1][0]) + abs(a_positions[i[3]][1] - i[2][0])] + [abs(b_positions[j[3]][0] - j[1][0]) + abs(b_positions[j[3]][1] - i[2][0])]); wire_length.append(l)
         if (i[0] == "U" or i[0] == "D") and (j[0] == "R" or j[0] == "L"):
             a, b, c, d = i[1][0] - j[1][0]  , i[1][1] - j[1][1], i[2][0] - j[2][0], i[2][1] - j[2][1]
             if a*b < 0 and c*d <0:
 
-                if i[0][0] == "L" or  i[0][0] == "R":m = min(m,  abs(i[1][0]) + abs(j[2][0])); print i, j, (i[1][0] , j[2][0]), a_positions[i[3]], b_positions[j[3]]
-                if i[0][0] == "U" or  i[0][0] == "D":m = min(m,  abs(i[2][0]) + abs(j[1][0])); print i, j, (i[2][0] , j[1][0]), a_positions[i[3]], b_positions[j[3]]
+                if i[0][0] == "L" or  i[0][0] == "R":m = min(m,  abs(i[1][0]) + abs(j[2][0]));l = sum([int(x[1:]) for x in a_wire[:i[3]]] + [int(x[1:]) for x in b_wire[:j[3]]] + [abs(a_positions[i[3]][0] - i[1][0]) + abs(a_positions[i[3]][1] - j[2][0])] + [abs(b_positions[j[3]][0] - i[1][0]) + abs(b_positions[j[3]][1] - j[2][0])]); wire_length.append(l)
+                if i[0][0] == "U" or  i[0][0] == "D":m = min(m,  abs(i[2][0]) + abs(j[1][0]));l = sum([int(x[1:]) for x in a_wire[:i[3]]] + [int(x[1:]) for x in b_wire[:j[3]]] + [abs(a_positions[i[3]][0] - j[1][0]) + abs(a_positions[i[3]][1] - i[2][0])] + [abs(b_positions[j[3]][0] - j[1][0]) + abs(b_positions[j[3]][1] - i[2][0])]); wire_length.append(l)
 
-print a_positions
-print b_positions
-#print m
-print sum([int(x[1:]) for x in a_wire])
-print [int(x[1:]) for x in a_wire]
-print wire_length
-
-
-
-
-
-
-
-
-
+print m
+print min(wire_length)
