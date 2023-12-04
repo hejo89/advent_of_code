9801f260f420662b49aab5ba90adb3c5190ad44c
Johannes <johannes.hess.89@gmail.com>
2023-12-04 21:31:18 +0100

    updated

--- 2023/04/main.py
+++ 2023/04/main.py
@@ -1,12 +1,10 @@
 import math
 import re
 import collections
-part_2 = collections.defaultdict(int)
-w = []
-s = set()
 r = 0
+q = collections.defaultdict(int)
 for idx, l in enumerate(open('input.txt'), 1):
-    part_2[idx] += 1
+    q[idx] += 1
     a, b = l.strip().split('|')
     w = re.findall(r'(\d+)', a)
     s = re.findall(r'(\d+)', b)
@@ -15,9 +13,9 @@ for idx, l in enumerate(open('input.txt'), 1):
         if c in s:
             p *= 2
     for c in range(idx, idx + min(int(math.log(p, 2)), 198 - idx)):
-        for n in range(part_2[idx]):
-            part_2[c + 1] += 1
+        for n in range(q[idx]):
+            q[c + 1] += 1
     r += p//2
 
 print(r)
-print(sum(part_2.values()))
+print(sum(q.values()))
