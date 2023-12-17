079692e67596032fb641bbc08d15950e3f57d21f
Johannes <johannes.hess.89@gmail.com>
2023-12-17 12:56:35 +0100

    day 07 part 2

--- 2023/05/main.py
+++ 2023/05/main.py
@@ -1,3 +1,9 @@
 inpt = (open("input.txt").read().strip().split("\n\n"))
 s = list(map(int, inpt.pop(0).split()[1:]))
+m = [[list(map(int, k.split())) for k in j.split('\n')[1:]] for j in inpt]
+
+
+
+
 print(s)
+print(m)
